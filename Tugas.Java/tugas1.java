df6c7f3cda61bdc8b907166773df65413df04b3c
farisfauzan <farisfauzan18@gmail.com>
2018-10-25 15:41:00 +0700

    Tugas1Dicoding

---
+++ Tugas.Java/tugas1.java
@@ -0,0 +1,13 @@
+class tugas1 {
+    
+    public static void main(String [] args){
+        
+        System.out.println("");
+        System.out.println("* * *");
+        System.out.println("* * *");
+        System.out.println("* * *");
+        System.out.println("* * *");
+        
+    }
+    
+}
---
+++ Tugas.Java/tugas3.java
@@ -0,0 +1,14 @@
+class tugas3 {
+    
+    public static void main(String [] args){
+        
+        System.out.println("");
+        System.out.println("    *  ");
+        System.out.println("    *  ");
+        System.out.println("  * * *");
+        System.out.println("  *   *");
+        System.out.println("* * * * *");
+        
+    }
+    
+}
